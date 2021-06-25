01332f6d09c3813e080a1c0283eb92e87e85d3e9
vivek <vivekmohan27@gmail.com>
2021-06-25 12:06:08 +0530

    feat: first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>VIVIVIVI</Text>
+      <Text>meetApp</Text>
       <StatusBar style="auto" />
     </View>
   );
